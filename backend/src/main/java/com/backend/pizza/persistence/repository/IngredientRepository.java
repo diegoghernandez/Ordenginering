c193b74d3ed6d3b4cfcaa51c18d7adb3be637515
DiegoJ4V <diegoj4v@gmail.com>
2024-02-16 17:17:33 -0600

    Move all files to a new directory pizza-data

--- backend/src/main/java/com/backend/pizza/persistence/repository/IngredientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.backend.pizza.persistence.repository;
-
-import com.backend.pizza.persistence.entity.IngredientEntity;
-import org.springframework.data.repository.ListCrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IngredientRepository extends ListCrudRepository<IngredientEntity, Integer> {
-}
--- backend/src/main/java/com/backend/pizza/web/dto/NecessaryValuesForChangeDto.java
+++
@@ -1,9 +0,0 @@
-package com.backend.pizza.web.dto;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
-public record NecessaryValuesForChangeDto(
-   @NotNull long id,
-   @NotBlank String password
-) {}
