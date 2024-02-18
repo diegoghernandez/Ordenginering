f584b9a48059fa9a4b561c9723bbad09291a33f6
DiegoJ4V <diegoj4v@gmail.com>
2024-02-18 13:51:24 -0600

    Implement spring gateway
    - Add servlet path to customer and data services

--- backend/pizza-customer/src/main/java/com/backend/pizzacustomer/web/CustomerController.java
+++ backend/pizza-customer/src/main/java/com/backend/pizzacustomer/web/CustomerController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/customer")
+@RequestMapping
 public class CustomerController {
 
    private final CustomerService customerService;
---
+++ backend/pizza-gateway/src/main/java/com/backend/pizzagateway/PizzaGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.backend.pizzagateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PizzaGatewayApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(PizzaGatewayApplication.class, args);
+   }
+
+}
