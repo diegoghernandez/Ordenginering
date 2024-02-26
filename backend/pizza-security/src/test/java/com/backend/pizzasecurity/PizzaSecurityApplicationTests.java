9f5b538e87421898794a494142fa65d96d9534b2
DiegoJ4V <diegoj4v@gmail.com>
2024-02-26 14:37:52 -0600

    Add pizza-security microservice, and update dependencies

---
+++ backend/pizza-security/src/test/java/com/backend/pizzasecurity/PizzaSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.pizzasecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaSecurityApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
