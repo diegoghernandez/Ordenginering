0cbfc0f5fca2cb970f25686af8263771fd6319e0
DiegoJ4V <diegoj4v@gmail.com>
2024-04-07 20:22:16 -0600

    Add hover and focus state to muliple buttons
    - Change dialog backdrop color
    
    Add focus and hover state to primary and secondary button styles, navbar and menu mobile buttons

--- frontend/src/components/order/ShoppingCart/index.tsx
+++ frontend/src/components/order/ShoppingCart/index.tsx
@@ -2,7 +2,11 @@ import { useShoppingCart } from '@/hooks/useShoppingCart'
 import { useEffect, useState } from 'react'
 import Styles from './ShoppingCart.module.css'
 
-export function ShoppingCart() {
+interface Props {
+   extraClass?: string;
+}
+
+export function ShoppingCart({ extraClass }: Props) {
    const [isOpen, setIsOpen] = useState(false)
    const pizzaList = useShoppingCart((state) => state.pizza)
    const addPizza = useShoppingCart((state) => state.addPizza)
@@ -21,7 +25,7 @@ export function ShoppingCart() {
    }, [])
    
    return (
-      <button aria-label='Shopping cart' onClick={() => setIsOpen(!isOpen)}>
+      <button className={extraClass} aria-label='Shopping cart' onClick={() => setIsOpen(!isOpen)}>
          <svg xmlns='http://www.w3.org/2000/svg' fill='none' stroke='currentColor' viewBox='0 0 24 24' className='humbleicons hi-cart'>
             <circle xmlns='http://www.w3.org/2000/svg' cx='7.5' cy='18.5' r='1.5' fill='currentColor'/><circle xmlns='http://www.w3.org/2000/svg' cx='16.5' cy='18.5' r='1.5' fill='currentColor'/><path xmlns='http://www.w3.org/2000/svg' stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M3 5h2l.6 3m0 0L7 15h10l2-7H5.6z'/>
          </svg>
