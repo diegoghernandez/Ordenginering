c15fb39cd99234fb63b4b33adb26f3c88d274b13
DiegoJ4V <diegoj4v@gmail.com>
2024-06-13 17:48:45 -0600

    Change findNavbarElements to include mobile and desktop menu

--- frontend/src/test/e2e/utils/navbarUtils.ts
+++ frontend/src/test/e2e/utils/navbarUtils.ts
@@ -1,13 +1,20 @@
 import {expect, type Page } from '@playwright/test'
 
 export async function findNavbarElements(page: Page) {
+   await page.setViewportSize({ width: 320, height: 900 })
    await page.getByLabel('Open menu').click()
    
    await expect(page.getByRole('link', { name: 'Home' })).toBeVisible()
    await expect(page.getByRole('link', { name: 'Menu' })).toBeVisible()
    await expect(page.getByRole('link', { name: 'Customize' })).toBeVisible()
-
+   
    await page.getByRole('button', { name: 'Close menu' }).click()
+   
+   await page.setViewportSize({ width: 1200, height: 900 })
+
+   await expect(page.getByRole('link', { name: 'Home' })).toBeVisible()
+   await expect(page.getByRole('link', { name: 'Menu' })).toBeVisible()
+   await expect(page.getByRole('link', { name: 'Customize' })).toBeVisible()
    
    await expect(page.getByRole('link', { name: 'Account' })).toBeVisible()
    await expect(page.getByLabel('Shopping cart')).toBeVisible()
