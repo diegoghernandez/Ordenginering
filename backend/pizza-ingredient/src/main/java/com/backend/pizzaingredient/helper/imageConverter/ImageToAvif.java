f0e42693589ccd49314a0a4d74ecbab1d7f78cf4
diegoghernandez <diegoj4v@gmail.com>
2024-07-14 14:10:22 -0600

    Replace File api for Path api in ImageToAvif class

--- backend/pizza-ingredient/src/main/java/com/backend/pizzaingredient/helper/imageConverter/ImageToAvif.java
+++ backend/pizza-ingredient/src/main/java/com/backend/pizzaingredient/helper/imageConverter/ImageToAvif.java
@@ -6,16 +6,17 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
+import java.net.URI;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Objects;
 
 public class ImageToAvif {
 
-   private static final String WORKING__DIRECTORY = new File("").getAbsolutePath() + "/src/main/java/com/backend/pizzaingredient/helper/imageConverter/";
+   private static final Path WORKING__DIRECTORY = Path.of("src", "main", "java", "com", "backend", "pizzaingredient", "helper", "imageConverter");
 
    public static byte[] converter(MultipartFile image) throws IOException, InterruptedException {
-      System.out.println(WORKING__DIRECTORY);
       Image resultingImage = ImageIO.read(image.getInputStream()).getScaledInstance(124, 112, Image.SCALE_DEFAULT);
       BufferedImage outputImage = new BufferedImage(124, 112, BufferedImage.TYPE_INT_RGB);
       outputImage.getGraphics().drawImage(resultingImage, 0, 0, null);
@@ -23,15 +24,16 @@ public class ImageToAvif {
       String originalNameWithoutExtension = Objects.requireNonNull(image.getOriginalFilename()).split("\\.")[0];
 
       ImageIO.write(outputImage, "jpg", new FileOutputStream(
-              WORKING__DIRECTORY + "images/" + originalNameWithoutExtension + ".jpg")
+              WORKING__DIRECTORY.toAbsolutePath() + "/images/" + originalNameWithoutExtension + ".jpg")
       );
 
       makeAvifImage(originalNameWithoutExtension);
 
-      new File(WORKING__DIRECTORY + "images/" + originalNameWithoutExtension + ".jpg").delete();
+      Files.delete(WORKING__DIRECTORY.resolve(Path.of("images", originalNameWithoutExtension + ".jpg")));
 
-      var avifFileBytes = Files.readAllBytes(Paths.get(WORKING__DIRECTORY + "images/" + originalNameWithoutExtension + ".avif"));
-      new File(WORKING__DIRECTORY + "images/" + originalNameWithoutExtension + ".avif").delete();
+      Path avifImage = Path.of("images", originalNameWithoutExtension + ".avif");
+      var avifFileBytes = Files.readAllBytes(WORKING__DIRECTORY.resolve(avifImage));
+      Files.delete(WORKING__DIRECTORY.resolve(avifImage));
 
       return avifFileBytes;
    }
@@ -44,7 +46,7 @@ public class ImageToAvif {
               "-j", "all", "-d", "10", "-y", "422", "--min", "36", "--max", "36", "--minalpha", "36", "--maxalpha", "36",
               "images/" + originalName + ".avif"
       );
-      builder.directory(new File(WORKING__DIRECTORY));
+      builder.directory(WORKING__DIRECTORY.toFile());
 
       var process = builder.start();
 
