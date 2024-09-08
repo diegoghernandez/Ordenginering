9a714cc092c8996c604df8d08ab2bfd43766a480
diegoghernandez <diegoj4v@gmail.com>
2024-09-08 16:04:22 -0600

    Add fallbackType to astro middleware

--- frontend/src/middleware/index.ts
+++ frontend/src/middleware/index.ts
@@ -2,23 +2,26 @@ import { middleware } from 'astro:i18n'
 import { defineMiddleware, sequence } from 'astro:middleware'
 
 const translationPathMiddleware = defineMiddleware((ctx, next) => {
-   const path = ctx.url.pathname
-   
-   if (!path.includes('/en') && !path.includes('/es')) {
-      // return Response.redirect(new URL('/client/en', ctx.url), 302)
-      const redirectPath = ctx.request.headers.get('accept-language')?.includes('es')
-         ? '/client/es'
-         : '/client/en'
-      return ctx.redirect(redirectPath, 302)
-   }
-   
-   return next()
+	const path = ctx.url.pathname
+
+	if (!path.includes('/en') && !path.includes('/es')) {
+		// return Response.redirect(new URL('/client/en', ctx.url), 302)
+		const redirectPath = ctx.request.headers
+			.get('accept-language')
+			?.includes('es')
+			? '/client/es'
+			: '/client/en'
+		return ctx.redirect(redirectPath, 302)
+	}
+
+	return next()
 })
 
 export const onRequest = sequence(
-   translationPathMiddleware,
-   middleware({
-      redirectToDefaultLocale: false,
-      prefixDefaultLocale: true
-   })
+	translationPathMiddleware,
+	middleware({
+		redirectToDefaultLocale: false,
+		prefixDefaultLocale: true,
+		fallbackType: 'rewrite',
+	})
 )
