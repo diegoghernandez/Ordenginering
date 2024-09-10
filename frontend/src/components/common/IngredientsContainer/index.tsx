e9d74394fa30cf00c37f57d15fb62cf20057c299
diegoghernandez <diegoj4v@gmail.com>
2024-09-10 16:21:05 -0600

    Change IngredientsContainer elements to a list

--- frontend/src/components/common/IngredientsContainer/index.tsx
+++ frontend/src/components/common/IngredientsContainer/index.tsx
@@ -2,24 +2,30 @@ import type { Ingredient, LocalesObject, LocalesString } from '@/types'
 import Styles from './IngredientsContainer.module.css'
 
 interface Props {
-   ingredients: Ingredient[]
-   local: string
+	ingredients: Ingredient[]
+	local: string
 }
 
 export function IngredientsContainer({ ingredients, local }: Props) {
-   return (
-      <div className={Styles['ingredients-container']}>
-         {ingredients.map((element) => (
-            <p key={crypto.randomUUID()}>{getLocalizedIngredients(element?.name, local)} <span>X{element.quantity}</span></p>
-         ))}
-      </div>
-   )
+	return (
+		<ul className={Styles['ingredients-container']}>
+			{ingredients.map((element) => (
+				<li key={crypto.randomUUID()}>
+					{getLocalizedIngredients(element?.name, local)}{' '}
+					<span>X{element.quantity}</span>
+				</li>
+			))}
+		</ul>
+	)
 }
 
-function getLocalizedIngredients(ingredientName: LocalesObject | undefined, locale: string) {
-   if (typeof ingredientName === 'string') {
-      return JSON.parse(ingredientName)[locale]
-   }
-   
-   return ingredientName?.[locale as LocalesString]
+function getLocalizedIngredients(
+	ingredientName: LocalesObject | undefined,
+	locale: string
+) {
+	if (typeof ingredientName === 'string') {
+		return JSON.parse(ingredientName)[locale]
+	}
+
+	return ingredientName?.[locale as LocalesString]
 }
