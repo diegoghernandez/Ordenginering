d0d97206511c6cf03f082a61d0fd8d61545117cc
diegoghernandez <diegoj4v@gmail.com>
2024-12-03 14:18:50 -0600

    Add mswServer to initialize and close msw servers

---
+++ frontend/src/test/e2e/utils/mswServer.ts
@@ -0,0 +1,34 @@
+import { authHandler } from '@/mocks/domains/authHandler'
+import { changeCustomerHandler } from '@/mocks/domains/changeCustomerHandler'
+import { customerHandler } from '@/mocks/domains/customerHandler'
+import { ingredientHandler } from '@/mocks/domains/ingredientHandler'
+import { jwtHandler } from '@/mocks/domains/jwtHandler'
+import { orderHandler } from '@/mocks/domains/orderHandler'
+import { createServer } from '@mswjs/http-middleware'
+import type { Server } from 'node:http'
+
+let httpCustomerServer: Server
+let httpIngredientServer: Server
+let httpJwtServer: Server
+let httpOrderServer: Server
+
+export function initializeMSWServers() {
+	httpCustomerServer = createServer(
+		undefined,
+		...customerHandler,
+		...authHandler,
+		...changeCustomerHandler
+	).listen(8765)
+	httpIngredientServer = createServer(undefined, ...ingredientHandler).listen(
+		2222
+	)
+	httpJwtServer = createServer(undefined, ...jwtHandler).listen(3000)
+	httpOrderServer = createServer(undefined, ...orderHandler).listen(4436)
+}
+
+export function closeMSWServers() {
+	httpCustomerServer.close()
+	httpIngredientServer.close()
+	httpJwtServer.close()
+	httpOrderServer.close()
+}
