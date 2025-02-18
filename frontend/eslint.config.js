745977f7ffcb9d4f5d8307dd671c674df05075de
diegoghernandez <diegoj4v@gmail.com>
2025-02-18 13:38:56 -0600

    Update eslint version from 8 to 9

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,39 +4,41 @@ import eslintHooks from 'eslint-plugin-react-hooks'
 import tsPlugin from '@typescript-eslint/eslint-plugin'
 import tsParser from '@typescript-eslint/parser'
 
-/** @type { import('eslint').Linter.FlatConfig[] } */
+/** @type { import('eslint').Linter.Config[] } */
 export default [
 	...eslintPluginAstro.configs.recommended,
-{
-	files: ['src/**/*.{ts,tsx}'],
-	ignores: ['dist/'],
-	languageOptions: {
-		parser: tsParser,
-		parserOptions: {
-			ecmaVersion: 'latest',
-			sourceType: 'module',
-			project: './tsconfig.json'
-		}
+	{
+		files: ['src/**/*.{ts,tsx}'],
+		ignores: ['dist/'],
+		languageOptions: {
+			parser: tsParser,
+			parserOptions: {
+				ecmaVersion: 'latest',
+				sourceType: 'module',
+				project: './tsconfig.json',
+			},
+		},
+		plugins: {
+			'@typescript-eslint': tsPlugin,
+			tsPlugin,
+			react: eslintReact,
+			'react-hooks': eslintHooks,
+		},
+		rules: {
+			...eslintHooks.configs.recommended.rules,
+			...tsPlugin.configs['eslint-recommended'].rules,
+			...tsPlugin.configs['recommended'].rules,
+			'react/react-in-jsx-scope': 'off',
+			'no-console': 'error',
+			'react/prop-types': 'off',
+			quotes: ['error', 'single', { avoidEscape: true }],
+			semi: ['error', 'never'],
+			'@typescript-eslint/triple-slash-reference': 'off',
+			'@typescript-eslint/no-floating-promises': 'error',
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{ ignoreRestSiblings: true },
+			],
+		},
 	},
-	plugins: {
-		'@typescript-eslint': tsPlugin,
-		tsPlugin,
-		react: eslintReact,
-		'react-hooks': eslintHooks
-	},
-	rules: {
-		...eslintHooks.configs.recommended.rules,
-		...tsPlugin.configs['eslint-recommended'].rules,
-      ...tsPlugin.configs['recommended'].rules,
-		'react/react-in-jsx-scope': 'off',
-		'no-console': 'error',
-		'react/prop-types': 'off',
-		'quotes': ['error', 'single', { 'avoidEscape': true }],
-		'semi': ['error', 'never'],
-		'@typescript-eslint/triple-slash-reference': 'off',
-		'@typescript-eslint/no-floating-promises': 'error',
-		'@typescript-eslint/no-unused-vars': [
-			'error', { 'ignoreRestSiblings': true }
-		]
-	}
-}]
+]
