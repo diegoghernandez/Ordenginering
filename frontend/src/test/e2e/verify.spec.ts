f16eb4a94888838a16b3c2a3bba54482ed612f71
diegoghernandez <diegoj4v@gmail.com>
2025-02-17 18:20:15 -0600

    Improve Verify e2e test

--- frontend/src/test/e2e/verify.spec.ts
+++ frontend/src/test/e2e/verify.spec.ts
@@ -7,19 +7,6 @@ LOCALES.forEach((locale) => {
 	const t = getJSON('../assets/i18n/pages/Verify.json')[locale]
 
 	test.describe(`${locale}: Verify page tests`, () => {
-		test('Should render the verify SUCCESSFUL page correctly', async ({
-			page,
-		}) => {
-			await page.goto(`/client/${locale}/verify?token=correct`)
-
-			await expect(page).toHaveTitle(t.seo.title)
-
-			await findNavbarElements(locale, page)
-
-			await expect(page.getByText(t.token.SUCCESSFUL)).toBeVisible()
-			await expect(page.getByRole('link', { name: 'Login' })).toBeVisible()
-		})
-
 		test('Should render the verify EXPIRED page correctly', async ({
 			page,
 		}) => {
@@ -31,8 +18,53 @@ LOCALES.forEach((locale) => {
 
 			await expect(page.getByText(t.token.EXPIRED)).toBeVisible()
 			await expect(
-				page.getByRole('button', { name: 'Resend token' })
+				page.getByRole('button', { name: t.resend, exact: true })
 			).toBeVisible()
+		})
+
+		test('Should send the token to the respective email and get a server error', async ({
+			page,
+		}) => {
+			await page.goto(`/client/${locale}/verify?token=failure`)
+
+			await expect(page).toHaveTitle(t.seo.title)
+
+			await page.getByRole('button', { name: t.resend, exact: true }).click()
+			await expect(
+				page.getByRole('button', { name: t.resend, exact: true })
+			).toBeDisabled()
+
+			await expect(page.getByText(t.responses.SERVER)).toBeVisible()
+		})
+
+		test('Should send the token to the respective email', async ({
+			page,
+		}) => {
+			await page.goto(`/client/${locale}/verify?token=expired`)
+
+			await expect(page).toHaveTitle(t.seo.title)
+
+			await page.getByRole('button', { name: t.resend, exact: true }).click()
+			await expect(
+				page.getByRole('button', { name: t.resend, exact: true })
+			).toBeDisabled()
+
+			await expect(page.getByText(t.responses.SUCCESS)).toBeVisible()
+		})
+
+		test('Should render the verify SUCCESSFUL page correctly', async ({
+			page,
+		}) => {
+			await Promise.all([
+				page.goto(`/client/${locale}/verify?token=correct`),
+
+				expect(page).toHaveTitle(t.seo.title),
+
+				findNavbarElements(locale, page),
+
+				expect(page.getByText(t.token.SUCCESSFUL)).toBeVisible(),
+				expect(page.getByRole('link', { name: t.login })).toBeVisible(),
+			])
 		})
 	})
 })
