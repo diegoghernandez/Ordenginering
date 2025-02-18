97d517e747e8edf61fa9d6ebfbc91c339faf4674
diegoghernandez <diegoj4v@gmail.com>
2025-02-17 18:18:55 -0600

    Add CORS_HEADERS constant

---
+++ frontend/src/constants/corsHeaders.ts
@@ -0,0 +1,6 @@
+export const CORS_HEADERS = Object.freeze({
+	'Access-Control-Allow-Origin': 'http://localhost:4321',
+	'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
+	'Access-Control-Allow-Headers': '*',
+	'Access-Control-Allow-Credentials': 'true',
+})
