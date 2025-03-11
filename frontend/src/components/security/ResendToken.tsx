69fffdeb6c5fbb32529f2465c3f1df6ee2160c5e
diegoghernandez <diegoj4v@gmail.com>
2025-03-10 19:35:28 -0600

    Add formulary with logic to reset the password

---
+++ frontend/src/components/security/ResendToken.tsx
@@ -0,0 +1,57 @@
+import { SmallModalContainer } from '@/components/common/SmallModalContainer'
+import { Spin } from '@/components/common/Spin'
+import { SECONDARY__BUTTON } from '@/constants/styles'
+import { useServicePromise } from '@/hooks/useServicePromise'
+import { resendToken } from '@/services/authService'
+import { useRef } from 'react'
+
+interface Props {
+	t: {
+		resendButton: string
+		responses: {
+			SUCCESS: string
+			SERVER: string
+		}
+	}
+}
+
+export function ResendToken({ t }: Props) {
+	const dialogRef = useRef<HTMLDialogElement>(null)
+	const { response, isLoading, handlePromise } = useServicePromise<
+		{ token: string },
+		string
+	>(resendToken)
+
+	const handleClick = () => {
+		dialogRef.current?.showModal()
+		handlePromise({
+			token:
+				new URLSearchParams(document.location.search).get('token') ?? '',
+		})
+	}
+
+	return (
+		<>
+			<button className={SECONDARY__BUTTON} onClick={handleClick}>
+				{t.resendButton}
+			</button>
+			<SmallModalContainer ref={dialogRef}>
+				{isLoading && !response ? (
+					<div
+						style={{ width: '64px', height: '64px', placeSelf: 'center' }}
+					>
+						<Spin />
+					</div>
+				) : (
+					<p>
+						{
+							t.responses[
+								response?.message as keyof Props['t']['responses']
+							]
+						}
+					</p>
+				)}
+			</SmallModalContainer>
+		</>
+	)
+}
---
+++ frontend/src/components/security/ResetPasswordForm.tsx
@@ -0,0 +1,99 @@
+import { CustomInput } from '@/components/common/CustomInput'
+import { FormContainer } from '@/components/common/FormContainer'
+import { ResendToken } from '@/components/security/ResendToken'
+import { useServicePromise } from '@/hooks/useServicePromise'
+import { resetPassword } from '@/services/authService'
+import type { TokenStatus, VerifyTokenDto } from '@/types'
+import { getFormValue } from '@/utils/getFormValue'
+
+interface Props {
+	t: {
+		submit: string
+		labels: {
+			newPassword: string
+			confirmNewPassword: string
+		}
+		responses: {
+			[key: string]: string
+		}
+		errors: {
+			[key: string]: string
+		}
+		resend: {
+			resendButton: string
+			SUCCESS: string
+		}
+	}
+}
+
+export function ResetPasswordForm({ t }: Props) {
+	const { response, isLoading, error, handlePromise, setError } =
+		useServicePromise<VerifyTokenDto, TokenStatus>(resetPassword)
+
+	const handleData = (formValues: FormData) => {
+		if (
+			getFormValue(t.labels.newPassword, formValues) !==
+			getFormValue(t.labels.confirmNewPassword, formValues)
+		) {
+			setError({
+				newPasswordError: t.errors['NOT_MATCH'],
+				repeatNewPasswordError: t.errors['NOT_MATCH'],
+			})
+		} else {
+			handlePromise({
+				token:
+					new URLSearchParams(document.location.search).get('token') ?? '',
+				newPassword: getFormValue(t.labels.newPassword, formValues),
+				repeatNewPassword: getFormValue(
+					t.labels.confirmNewPassword,
+					formValues
+				),
+			})
+		}
+	}
+
+	return (
+		<>
+			<FormContainer
+				handleData={handleData}
+				response={
+					response
+						? {
+								status: response.status,
+								message: response.message
+									? t.responses[response.message]
+									: '',
+						  }
+						: response
+				}
+				submitButton={{ isLoading, label: t.submit }}
+			>
+				<CustomInput
+					label={t.labels.newPassword}
+					type='password'
+					required={true}
+					error={error?.newPasswordError}
+					disable={isLoading}
+				/>
+				<CustomInput
+					label={t.labels.confirmNewPassword}
+					type='password'
+					required={true}
+					error={error?.repeatNewPasswordError}
+					disable={isLoading}
+				/>
+			</FormContainer>
+			{response?.message === 'EXPIRED' ? (
+				<ResendToken
+					t={{
+						resendButton: t.resend.resendButton,
+						responses: {
+							SUCCESS: t.resend.SUCCESS,
+							SERVER: t.responses.SERVER,
+						},
+					}}
+				/>
+			) : null}
+		</>
+	)
+}
