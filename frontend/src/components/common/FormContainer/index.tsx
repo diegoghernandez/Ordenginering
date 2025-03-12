0583f15586f574298a85718a5afa2ee6b00bdb58
diegoghernandez <diegoj4v@gmail.com>
2025-03-11 18:29:25 -0600

    Add stopPropagation method to FormContainer

--- frontend/src/components/common/FormContainer/index.tsx
+++ frontend/src/components/common/FormContainer/index.tsx
@@ -4,39 +4,48 @@ import type { FormEvent, ReactElement } from 'react'
 import './FormContainer.module.css'
 
 interface Props {
-   handleData: (formValues: FormData) => void;
-   response: {
-      status: number;
-      message: string;
-   } | null;
-   children?: Array<ReactElement> | ReactElement;
-   submitButton: {
-      label: string;
-      isLoading: boolean;
-   };
+	handleData: (formValues: FormData) => void
+	response: {
+		status: number
+		message: string
+	} | null
+	children?: Array<ReactElement> | ReactElement
+	submitButton: {
+		label: string
+		isLoading: boolean
+	}
 }
 
 export function FormContainer({
-   handleData,
-   response,
-   children,
-   submitButton
+	handleData,
+	response,
+	children,
+	submitButton,
 }: Props) {
+	const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+		event.stopPropagation()
 
-   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {      
-      if (event.currentTarget instanceof HTMLFormElement) {
-         handleData(new FormData(event.currentTarget))
-      }
-   }
+		if (event.currentTarget instanceof HTMLFormElement) {
+			handleData(new FormData(event.currentTarget))
+		}
+	}
 
-   return (
-      <form onSubmit={handleSubmit}>
-         {response?.message ? <Callout type={response?.status !== 200 ? 'error' : 'success'} message={response?.message} /> : null}
-         {children}
-         <button
-            className={PRIMARY__BUTTON}
-            disabled={submitButton.isLoading}
-         >{submitButton.label}</button>
-      </form>
-   )
+	return (
+		<form onSubmit={handleSubmit}>
+			{response?.message ? (
+				<Callout
+					type={response?.status !== 200 ? 'error' : 'success'}
+					message={response?.message}
+				/>
+			) : null}
+			{children}
+			<button
+				type='submit'
+				className={PRIMARY__BUTTON}
+				disabled={submitButton.isLoading}
+			>
+				{submitButton.label}
+			</button>
+		</form>
+	)
 }
