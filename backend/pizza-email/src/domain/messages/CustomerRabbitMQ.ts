7f9cf27566ec631d85f3ad506ae569e9a032c82c
diegoghernandez <diegoj4v@gmail.com>
2025-03-18 19:27:13 -0600

    Change CustomerRabbitMQ logic
    - Add onResetPassword method
    
    - Add processMessage helper
    
    - Implement processMessage helper in the method onSaveCustomer

--- backend/pizza-email/src/domain/messages/CustomerRabbitMQ.ts
+++ backend/pizza-email/src/domain/messages/CustomerRabbitMQ.ts
@@ -1,6 +1,9 @@
-import { Channel } from 'amqplib'
+import { Channel, ConsumeMessage } from 'amqplib'
 import { CustomerMessage } from '../../contracts/messages/CustomerMessage.js'
-import { CustomerEmail } from '../../contracts/services/CustomerEmail.js'
+import {
+	CustomerEmail,
+	EmailParams,
+} from '../../contracts/services/CustomerEmail.js'
 
 export class CustomerRabbitMQ implements CustomerMessage {
 	#channel: Channel
@@ -13,22 +16,73 @@ export class CustomerRabbitMQ implements CustomerMessage {
 
 	async initialize() {
 		await this.#onSaveCustomer()
+		await this.#onResetPassword()
 	}
 
 	async #onSaveCustomer() {
 		const channel = this.#channel
 		const queue = 'q.pizza_customer.welcome_email'
 
-		await channel.consume(queue, async (msg) => {
-			if (msg?.content) {
-				const emailParams = JSON.parse(msg.content.toString())
+		await channel.consume(
+			queue,
+			async (msg) => {
+				const emailParams = this.#processMessage<EmailParams>(channel, msg)
+				if (emailParams && msg) {
+					this.#customerEmail.sendWelcome({
+						email: emailParams.email,
+						locale: emailParams.locale,
+						token: emailParams.token,
+					})
 
-				await this.#customerEmail.sendWelcome({
-					email: emailParams.email,
-					locale: emailParams.locale,
-					token: emailParams.token,
-				})
-			}
-		})
+					channel.ack(msg)
+				}
+			},
+			{ noAck: false }
+		)
+	}
+
+	async #onResetPassword() {
+		const channel = this.#channel
+		const queue = 'q.pizza_customer.reset_password_email'
+
+		await channel.consume(
+			queue,
+			async (msg) => {
+				const emailParams = this.#processMessage<EmailParams>(channel, msg)
+				if (emailParams && msg) {
+					channel.ack(msg)
+
+					await this.#customerEmail.resetPassword({
+						email: emailParams.email,
+						locale: emailParams.locale,
+						token: emailParams.token,
+					})
+				}
+			},
+			{ noAck: false }
+		)
+	}
+
+	#processMessage<T>(
+		channel: Channel,
+		msg: ConsumeMessage | null
+	): T | undefined {
+		if (!msg) return
+		if (!msg.content) {
+			channel.reject(msg)
+			return
+		}
+
+		let msgContent
+
+		try {
+			msgContent = JSON.parse(msg.content.toString())
+		} catch (error) {
+			console.error('Process message error:', error?.message)
+			channel.reject(msg)
+			return
+		}
+
+		return msgContent
 	}
 }
--- backend/pizza-email/src/test/messages/CustomerRabbitMQ.test.tsx
+++ backend/pizza-email/src/test/messages/CustomerRabbitMQ.test.tsx
@@ -1,64 +1,127 @@
-import { connect } from 'amqplib'
-import { afterEach, beforeEach } from 'node:test'
+import { Channel, connect } from 'amqplib'
 import { Resend } from 'resend'
-import { describe, expect, it, vi } from 'vitest'
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import { RABBIT_CONFIG } from '../../constants/RabbitConfig.js'
 import { CustomerRabbitMQ } from '../../domain/messages/CustomerRabbitMQ.js'
 import { CustomerResend } from '../../domain/services/CustomerResend.js'
-import Welcome from '../../emails/Welcome.js'
-import { render } from '@react-email/components'
-import { cleanup } from '@testing-library/react'
 
 const WELCOME_QUEUE = 'q.pizza_customer.welcome_email'
+const RESET_PASSWORD_QUEUE = 'q.pizza_customer.reset_password_email'
 
-const connection = await connect(RABBIT_CONFIG)
-const channel = await connection.createChannel()
-await channel.assertExchange('e.pizza_customer.saved', 'fanout')
-await channel.assertQueue(WELCOME_QUEUE, {
-	durable: false,
-	autoDelete: true,
-})
-await channel.bindQueue(WELCOME_QUEUE, 'e.pizza_customer.saved', '')
+let channel!: Channel
+
+const customerResend = new CustomerResend(new Resend('resend_key'))
+const data = {
+	email: 'test@test.test',
+	locale: 'es',
+	token: 'r2qrv321vbg',
+}
+
+vi.spyOn(customerResend, 'sendWelcome')
+vi.spyOn(customerResend, 'resetPassword')
 
 describe('CustomerMessage tests', () => {
-	beforeEach(async () => await channel.purgeQueue(WELCOME_QUEUE))
+	beforeEach(async () => {
+		const connection = await connect(RABBIT_CONFIG)
+		channel = await connection.createChannel()
+		await channel.assertExchange('e.pizza_customer.saved', 'fanout')
+		await channel.assertQueue(WELCOME_QUEUE, {
+			durable: false,
+			autoDelete: true,
+		})
+		await channel.bindQueue(WELCOME_QUEUE, 'e.pizza_customer.saved', '')
 
-	afterEach(() => cleanup())
+		await channel.assertExchange('e.pizza_customer.reset_password', 'fanout')
+		await channel.assertQueue(RESET_PASSWORD_QUEUE, {
+			durable: false,
+			autoDelete: true,
+		})
+		await channel.bindQueue(
+			RESET_PASSWORD_QUEUE,
+			'e.pizza_customer.reset_password',
+			''
+		)
 
-	it('Should verify if the queue "q.pizza_customer.welcome_email" is consumed with the desire data', async () => {
-		const resend = new Resend(process.env.RESEND_KEY)
-		const customerResend = new CustomerResend(resend)
 		await new CustomerRabbitMQ(channel, customerResend).initialize()
-		const data = {
-			email: 'test@test.test',
-			locale: 'es',
-			token: 'r2qrv321vbg',
-		}
 
-		vi.spyOn(customerResend, 'sendWelcome')
-		vi.spyOn(resend.emails, 'send')
+		vi.resetAllMocks()
+	})
 
-		await vi.waitFor(async () => {
-			expect(customerResend.sendWelcome).toBeCalledTimes(0)
-			expect(customerResend.sendWelcome).not.toHaveBeenCalledWith(data)
-			expect(resend.emails.send).toBeCalledTimes(0)
+	afterEach(async () => {
+		await channel.close()
+	})
+
+	describe('Queue q.pizza_customer.welcome_email tests', () => {
+		it('Should sendWelcome not be called if is not a json and clear the queue', async () => {
+			await vi.waitFor(async () => {
+				expect(customerResend.sendWelcome).toBeCalledTimes(0)
+				expect(customerResend.sendWelcome).not.toHaveBeenCalledWith(data)
+			})
+
+			channel.sendToQueue(WELCOME_QUEUE, Buffer.from('failure'))
+
+			await vi.waitFor(async () => {
+				const countQueue = await channel.checkQueue(WELCOME_QUEUE)
+				expect(countQueue?.messageCount).toBe(0)
+
+				expect(customerResend.sendWelcome).not.toBeCalledTimes(1)
+				expect(customerResend.sendWelcome).not.toHaveBeenCalledWith(data)
+			})
 		})
 
-		channel.sendToQueue(WELCOME_QUEUE, Buffer.from(JSON.stringify(data)))
+		it('Should verify if the queue is consumed by sendWelcome with the desire data and clear the message', async () => {
+			await vi.waitFor(async () => {
+				expect(customerResend.sendWelcome).toBeCalledTimes(0)
+				expect(customerResend.sendWelcome).not.toHaveBeenCalledWith(data)
+			})
 
-		const html = await render(
-			<Welcome token={data.token} locale={data.locale as 'es'} />
-		)
-		await vi.waitFor(() => {
-			expect(customerResend.sendWelcome).toBeCalledTimes(1)
-			expect(customerResend.sendWelcome).toHaveBeenCalledWith(data)
-			expect(resend.emails.send).toBeCalledTimes(1)
-			expect(resend.emails.send).toHaveBeenCalledWith({
-				from: 'onboarding@rd34124esend.dev',
-				to: 'test@test.test',
-				subject: 'Hello World',
-				html,
-				react: <Welcome token={data.token} locale={data.locale as 'es'} />,
+			channel.sendToQueue(WELCOME_QUEUE, Buffer.from(JSON.stringify(data)))
+
+			await vi.waitFor(async () => {
+				const checkQueue = await channel.checkQueue(WELCOME_QUEUE)
+				expect(checkQueue.messageCount).not.toBe(1)
+
+				expect(customerResend.sendWelcome).toBeCalledTimes(1)
+				expect(customerResend.sendWelcome).toHaveBeenCalledWith(data)
+			})
+		})
+	})
+
+	describe('Queue q.pizza_customer.reset_password_email tests', () => {
+		it('Should resetPassword not be called if is not a json and clear the message', async () => {
+			await vi.waitFor(async () => {
+				expect(customerResend.resetPassword).toBeCalledTimes(0)
+				expect(customerResend.resetPassword).not.toHaveBeenCalledWith(data)
+			})
+
+			channel.sendToQueue(RESET_PASSWORD_QUEUE, Buffer.from('failure'))
+
+			await vi.waitFor(async () => {
+				const countQueue = await channel.checkQueue(RESET_PASSWORD_QUEUE)
+				expect(countQueue.messageCount).not.toBe(1)
+
+				expect(customerResend.resetPassword).not.toBeCalledTimes(1)
+				expect(customerResend.resetPassword).not.toHaveBeenCalledWith(data)
+			})
+		})
+
+		it('Should verify if the queue is consumed by resetPassword with the desire data and clear the message', async () => {
+			await vi.waitFor(async () => {
+				expect(customerResend.resetPassword).toBeCalledTimes(0)
+				expect(customerResend.resetPassword).not.toHaveBeenCalledWith(data)
+			})
+
+			channel.sendToQueue(
+				RESET_PASSWORD_QUEUE,
+				Buffer.from(JSON.stringify(data))
+			)
+
+			await vi.waitFor(async () => {
+				const checkQueue = await channel.checkQueue(RESET_PASSWORD_QUEUE)
+				expect(checkQueue.messageCount).toBe(0)
+
+				expect(customerResend.resetPassword).toBeCalledTimes(1)
+				expect(customerResend.resetPassword).toHaveBeenCalledWith(data)
 			})
 		})
 	})
