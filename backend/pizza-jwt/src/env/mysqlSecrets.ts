b1e5c32d90715428d5b97ba852ef5b9ca557e924
diegoghernandez <diegoj4v@gmail.com>
2025-04-27 12:42:24 -0600

    Fix MYSQL_DOMAIN value

--- backend/pizza-jwt/src/env/mysqlSecrets.ts
+++ backend/pizza-jwt/src/env/mysqlSecrets.ts
@@ -1,6 +1,8 @@
+const [MYSQL_DOMAIN, MYSQL_PORT] = globalThis.process.env.MYSQL_DOMAIN?.split(':') ?? [null, null]
+
 export const MYSQL_SECRETS = Object.freeze({
-   MYSQL_DOMAIN: globalThis.process.env.MYSQL_DOMAIN,
-   MYSQL_PORT: globalThis.process.env.MYSQL_DOMAIN?.split(':')[1],
+   MYSQL_DOMAIN,
+   MYSQL_PORT,
    MYSQL_USERNAME: globalThis.process.env.MYSQL_USERNAME,
    MYSQL_PASSWORD: globalThis.process.env.MYSQL_PASSWORD
 })
